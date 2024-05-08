e6544b160669c049faabdabd4f5471500cc512e6
thuongla <thuongla@imt-soft.com>
2024-05-08 11:03:37 +0700

    update 1

--- src/server.js
+++ src/server.js
@@ -1,20 +1,23 @@
-const express = require('express')
-const path = require('path')
+require('dotenv').config();
+
+const express = require('express');
+const path = require('path');
+
+const configViewEngine = require('./config/viewsEngine');
+const webRoutes = require('./routes/web');
 
 const env = path.join(process.cwd(), 'config', '.env');
-require("dotenv").config({ path: env });
 
+// config server
 const app = express()
 const port = process.env.PORT || 8888;
 const hostname = process.env.HOST_NAME;
 
 //config template engine
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+configViewEngine(app);
 
-app.get('/', (req, res) => {
-  res.render('sample.ejs')
-})
+// khai bao routes
+app.use('/', webRoutes);
 
 app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`)
